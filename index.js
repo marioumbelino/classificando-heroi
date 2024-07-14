36a3b15fefe9ea593427f422d685bfbe46cfe880
marioumbelino <marioherbert2016@gmail.com>
2024-07-13 22:02:03 -0300

    Resolução do Desafio

---
+++ index.js
@@ -0,0 +1,22 @@
+let nomeHeroi = "Umbelino"
+let xp = 6430
+
+if (xp < 1000) {
+    nivel = "Ferro"
+} else if (xp < 2000) {
+    nivel = "Bronze"
+} else if (xp < 5000) {
+    nivel = "Prata"
+} else if (xp < 7000) {
+    nivel = "Ouro"
+} else if (xp < 8000) {
+    nivel = "Platina"
+} else if (xp < 9000) {
+    nivel = "Ascendente"
+} else if (xp < 10000) {
+    nivel = "Imortal"
+} else {
+    nivel = "Radiante"
+}
+
+console.log(`O herói de nome ${nomeHeroi} está no nível ${nivel}`);
